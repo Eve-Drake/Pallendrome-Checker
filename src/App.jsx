baf33abf406a18ae626d957c560e4fda4c363e0b
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-07 12:19:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [word, setWord] = useState('')
+  const [reversedWord, setReversedWord] = useState('')
+
+  const reverse = () =>{
+    setReversedWord('')
+    let newStr = ''
+    for(let i = word.length - 1; i >= 0; i--){
+      newStr += word[i]
+    }
+    setReversedWord(newStr)
+  }
+
+  return (
+    <div className="App">
+      <input 
+      placeholder='Enter a word'
+      value={word}
+      onChange={(e)=>setWord(e.target.value)}
+      />
+     <button onClick={reverse}>Reverse</button>
+     <div>
+      {reversedWord && (reversedWord.toLocaleLowerCase() === word.toLocaleLowerCase()? <h1>{word} is a pallendrome</h1>: <h1>{word} is not a pallendrome</h1>)}
+     </div>
+     
+    </div>
+  )
+}
+
+export default App
